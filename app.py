331810b5c32b02853c5c370c0f6d1c44f0700561
Fahad <fahad@Fahads-Air.lan>
2023-08-02 10:27:03 +0200

    Fixation des probleme Flask et probleme de lien avec gcloud

--- app.py
+++ app.py
@@ -9,9 +9,20 @@ def index():
     template_dir = os.path.abspath('/Users/fahad/Documents/Projet/templates/')
     return render_template('index.html', template_folder=template_dir)
 
+@app.route('/moyenne_expo.html')
 def Moyenne_expo():
     template_dir = os.path.abspath('/Users/fahad/Documents/Projet/templates/')
     return render_template('moyenne_expo.html', template_folder=template_dir)
+
+@app.route('/analyse.html')
+def analyse():
+    template_dir = os.path.abspath('/Users/fahad/Documents/Projet/templates/')
+    return render_template('analyse.html', template_folder=template_dir)
+
+@app.route('/croisement_moyennes_mobiles.html')
+def croisement():
+    template_dir = os.path.abspath('/Users/fahad/Documents/Projet/templates/')
+    return render_template('croisement_moyennes_mobiles.html', template_folder=template_dir)
 
 if __name__ == '__main__':
     app.run(debug=True)
